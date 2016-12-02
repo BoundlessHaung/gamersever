f86c9d16e68bea57726b8868430e1e768fb7ecc3
BoundlessHaung <george.haung@sandboxcn.com>
2016-12-02 09:27:34 +0800

    User类
    1. 解决跨域问题
    2. 增加HTTP登录验证
    3. 注册增加去重

--- http/application/api/controller/User.php
+++ http/application/api/controller/User.php
@@ -5,12 +5,62 @@ use think\Validate;
 use app\common\model\User as Users;
 use think\helper\Str;
 use think\helper\Hash;
+use Predis\Client;
+
 
 class User
 {
+
+    public function __construct()
+    {
+        // 解决跨域问题
+        header("Access-Control-Allow-Origin: *");
+    }
+
+
     public function index()
     {
         return "Manor: chaos";
+    }
+
+    public function signin()
+    {
+        // 判断所需参数是否全部存在，缺失则结束
+        if (!isset($_POST['username']) || !isset($_POST['password'])) {
+            return json([
+                'status' => 0,
+                'msg' => '传入参数错误'
+            ]);
+        }
+        // 获取需要登录的用户
+        $user = Users::get(['username' => $_POST['username']]);
+        // 不存在则结束
+        if (!$user) {
+            return json([
+                'status' => 0,
+                'msg' => '用户不存在'
+            ]);
+        }
+        // 对比密码
+        if (Hash::check($_POST['password'], $user['password'], 'Md5', ['salt'  =>  $user['salt']])) {
+            // 更新IP
+            $clientIp = $_SERVER["REMOTE_ADDR"];
+            $user->lastsigninip = $clientIp;
+            $user->save();
+            $token = Hash::make($user['username'], 'Md5', ['salt'  =>  $clientIp]);
+            $redisClient = new Client('tcp://127.0.0.1:6379');
+            $redisClient->set('signin_' . $user['username'], $token);
+            return json([
+                'status' => 1,
+                'msg' => '用户验证成功',
+                'token'=> $token
+            ]);
+        } else {
+            return json([
+                'status' => 0,
+                'msg' => '你个笨蛋又没记住密码！'
+            ]);
+        }
     }
 
     /**
@@ -25,6 +75,12 @@ class User
                 'msg' => '传入参数错误'
             ]);
     	}
+        if (Users::get(['username' => $_POST['username']])) {
+            return json([
+                'status' => 0,
+                'msg' => '用户已经存在'
+            ]);
+        }
     	// 获取参数存入data
     	$data['username'] = $_POST['username'];
     	$data['password'] = $_POST['password'];
@@ -62,4 +118,5 @@ class User
             ]);
         }
     }
+
 }
