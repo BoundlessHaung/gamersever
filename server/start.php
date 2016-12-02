5d38b0f0a28c2fc82c80c3c3fa3753f38c73b187
BoundlessHaung <george.haung@sandboxcn.com>
2016-12-02 16:32:25 +0800

    连接后未登录会被断开连接，登陆后映射到playerlist

--- server/start.php
+++ server/start.php
@@ -12,6 +12,7 @@
 namespace server;
 
 use Workerman\Worker;
+use Workerman\Lib\Timer;
 use Predis\Client;
 use think\helper\Str;
 
@@ -21,18 +22,32 @@ $mc_server = new Worker("websocket://0.0.0.0:2346");
 // 启动1个进程对外提供服务
 $mc_server->count = 1;
 
-// 为服务器增加一个属性用来存储在线玩家（映射 uid=>$connection）
+// 为服务器增加一个属性用来存储在线玩家（映射 username=>$connection）
 $mc_server->playerlist = array();
 
 // 客户端连接服务器
 $mc_server->onConnect = function($connection)
 {
-    echo "IP为：" . $connection->getRemoteIp() . "的用户连接到了服务器\n";
+	global $mc_server;
+	echo "IP为：" . $connection->getRemoteIp() . "的用户连接到了服务器\n";
+	Timer::add(2, function($connection, $mc_server)
+	{
+		if ($connection->username) {
+			if ($mc_server->playerlist[$connection->username]) {
+				return;
+			}
+			echo "IP为：" . $connection->getRemoteIp() . "的客户端逾期未做登录操作，已强制断开连接\n";
+			return $connection->close();
+		}
+		// 如果能运行到这里那么就断开链接。
+		return $connection->close();
+	}, array($connection, $mc_server), false);
 };
-
+	
 // 当收到客户端发来的数据时
 $mc_server->onMessage = function($connection, $data)
 {
+	global $mc_server;
 	echo "IP为：" . $connection->getRemoteIp() . "发来消息：" . $data . "\n";
 	// 格式化接收到的数据
 	$clientMsg = json_decode($data, true);
@@ -43,13 +58,16 @@ $mc_server->onMessage = function($connection, $data)
 			// 进行对比
 			$redisClient = new Client('tcp://127.0.0.1:6379');
 			if ($clientMsg['token'] == $redisClient->get('signin_' . $clientMsg['username'])) {
-				$connection->send(json_encode([
+				// 将用户名存入链接并以用户名为key将链接的客户端映射到playerlist中
+				$connection->username = $clientMsg['username'];
+				$mc_server->playerlist[$clientMsg['username']] = $connection;
+				return $connection->send(json_encode([
 					'type' => 'login',
 					'status' => 1,
 					'msg' => '登录成功'
 				]));
 			} else {
-				$connection->send(json_encode([
+				return $connection->send(json_encode([
 					'type' => 'login',
 					'status' => 0,
 					'msg' => '登录失败，链接断开'
