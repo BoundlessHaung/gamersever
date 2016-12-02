d3242c2c477a9c428aa63ba0d0befcc4fdfcc8a2
BoundlessHaung <george.haung@sandboxcn.com>
2016-12-02 09:30:06 +0800

    结构调整，改为入口文件

--- server.php
+++ server.php
@@ -1,26 +1,17 @@
 <?php
-use Workerman\Worker;
-require_once 'workerman/Autoloader.php';
+// +----------------------------------------------------------------------
+// | Manor: chaos —————— Times war strategy game
+// +----------------------------------------------------------------------
+// | Manor: chaos GameServer
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2016 http://xxxxxxxx.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Boundless <george.haung@sandboxcn.com>
+// +----------------------------------------------------------------------
 
-// 创建一个Worker监听2346端口，使用websocket协议通讯
-$ws_worker = new Worker("websocket://0.0.0.0:2346");
+// [ 应用入口文件 ]
 
-// 启动1个进程对外提供服务
-$ws_worker->count = 1;
-
-// 客户端连接服务器
-$ws_worker->onConnect = function($connection)
-{
-    echo "IP为：" . $connection->getRemoteIp() . "的用户连接到了服务器\n";
-};
-
-// 当收到客户端发来的数据后返回hello $data给客户端
-$ws_worker->onMessage = function($connection, $data)
-{
-	echo "IP为：" . $connection->getRemoteIp() . "发来消息：" . $data . "\n";
-    // 向客户端发送hello $data
-    $connection->send('Hello！我们收到了：' . $data);
-};
-
-// 运行worker
-Worker::runAll();
+// 注册Composer依赖管理自动加载
+require 'vendor/autoload.php';
+// 加载服务器引导文件
+require __DIR__ . '/server/start.php';
