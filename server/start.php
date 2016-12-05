c43585c16182456340bb17cb4ba4f4b1fca6a767
BoundlessHaung <george.haung@sandboxcn.com>
2016-12-05 14:46:39 +0800

    本身不再有处理能力

--- server/start.php
+++ server/start.php
@@ -14,7 +14,7 @@ namespace server;
 use Workerman\Worker;
 use Workerman\Lib\Timer;
 use Predis\Client;
-use think\helper\Str;
+use server\controller;
 
 // 创建一个Worker监听2346端口，使用websocket协议通讯
 $mc_server = new Worker("websocket://0.0.0.0:2346");
@@ -25,7 +25,12 @@ $mc_server->count = 1;
 // 为服务器增加一个属性用来存储在线玩家（映射 username=>$connection）
 $mc_server->playerlist = array();
 
-// 客户端连接服务器
+// redis常驻内存
+$mc_server->redis = new Client('tcp://127.0.0.1:6379');
+
+/**
+ * 客户端连接服务器
+ */
 $mc_server->onConnect = function($connection)
 {
 	global $mc_server;
@@ -43,42 +48,16 @@ $mc_server->onConnect = function($connection)
 		return $connection->close();
 	}, array($connection, $mc_server), false);
 };
-	
-// 当收到客户端发来的数据时
+
+/**
+ * 当收到客户端发来的数据时
+ */
 $mc_server->onMessage = function($connection, $data)
 {
 	global $mc_server;
 	echo "IP为：" . $connection->getRemoteIp() . "发来消息：" . $data . "\n";
-	// 格式化接收到的数据
-	$clientMsg = json_decode($data, true);
-	switch ($clientMsg['type']) {
-		case 'login':
-			echo "username：" . $clientMsg['username'] . "\n";
-			echo "token：" . $clientMsg['token'] . "\n";
-			// 进行对比
-			$redisClient = new Client('tcp://127.0.0.1:6379');
-			if ($clientMsg['token'] == $redisClient->get('signin_' . $clientMsg['username'])) {
-				// 将用户名存入链接并以用户名为key将链接的客户端映射到playerlist中
-				$connection->username = $clientMsg['username'];
-				$mc_server->playerlist[$clientMsg['username']] = $connection;
-				return $connection->send(json_encode([
-					'type' => 'login',
-					'status' => 1,
-					'msg' => '登录成功'
-				]));
-			} else {
-				return $connection->send(json_encode([
-					'type' => 'login',
-					'status' => 0,
-					'msg' => '登录失败，链接断开'
-				]))->close();
-			}			
-			break;
-		default:
-			$connection->send('ERROR：未知的数据类型。');
-			break;
-	}
-
+	$serverController = new controller($mc_server, $connection, $data);
+	$serverController->doIt();
 };
 
 // 运行worker
